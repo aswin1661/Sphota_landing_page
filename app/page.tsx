f7b6c15cc7bb57e5f4eb1fdd1c442d75df90cfe4
Aswin S <aswinsanthoshachus@gmail.com>
2025-07-27 01:49:56 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Home from "./components/home";
 
-export default function Page() {
+export default async function Page() {
 await new Promise((resolve) => setTimeout(resolve, 1000));
   return (
     <div>
