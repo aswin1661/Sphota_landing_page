aa074172f40aeabebae2417ed47e9e51b5855fab
Aswin S <aswinsanthoshachus@gmail.com>
2025-07-27 01:30:44 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-black">{children}</body>
+      <body className="bg-black h-full w-screen">{children}</body>
     </html>
   )
 }
