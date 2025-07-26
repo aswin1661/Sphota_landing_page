58fcc743c52834fb6492c681250a7ea04d0e5144
Aswin S <aswinsanthoshachus@gmail.com>
2025-07-27 01:16:01 +0530

    Create loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,17 @@
+
+import styles from './Loader.module.css';
+
+const Loader = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.circle}></div>
+      <div className={styles.circle}></div>
+      <div className={styles.circle}></div>
+      <div className={styles.shadow}></div>
+      <div className={styles.shadow}></div>
+      <div className={styles.shadow}></div>
+    </div>
+  );
+};
+
+export default Loader;
