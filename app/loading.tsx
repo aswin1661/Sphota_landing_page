b9ef453d37712e469bc24cd4719ce6eb765f1c8b
Aswin S <aswinsanthoshachus@gmail.com>
2025-07-27 01:47:13 +0530

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,15 +1,7 @@
-
-'use client';
-import { useEffect, useState } from 'react';
 import styles from './Loader.module.css';
-export default function Loading() {
-  const [show, setShow] = useState(true);
 
-  useEffect(() => {
-    const timeout = setTimeout(() => setShow(false), 1000); // 1 second delay
-    return () => clearTimeout(timeout);
-  }, []);
-  return show? (
+export default function Loading() {
+  return (
     <div className={styles.wrapper}>
       <div className={styles.circle}></div>
       <div className={styles.circle}></div>
@@ -18,5 +10,5 @@ export default function Loading() {
       <div className={styles.shadow}></div>
       <div className={styles.shadow}></div>
     </div>
-  ):null;
+  );
 }
