025dd196e90368d0ad4f5888aaf1f25199a1e529
aswin1661 <aswinsanthoshachus@gmail.com>
2025-08-04 16:22:44 +0530

    home update

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -76,19 +76,19 @@ export default function Home() {
 ></div>
 
         {/* Heading */}
-        <h1 className="absolute pt-[25vh] berserker text-5xl animated animatedFadeInUp fadeInUp text-white drop-shadow-md z-30">
+        <h1 className="absolute pt-[20vh] berserker text-5xl animated animatedFadeInUp fadeInUp text-white drop-shadow-md z-30">
           SPHOTA
         </h1>
 
         {/* Description */}
-        <h1 className="absolute text-center pt-[40vh] tahoma animated animatedFadeInUp fadeInUp text-white text-[1.9vh] lg:text-[2vh] px-[5vw] z-30">
+        <h1 className="absolute text-center pt-[35vh] tahoma animated animatedFadeInUp fadeInUp text-white text-[1.9vh] lg:text-[2vh] px-[5vw] z-30">
           24 hours. Your sphota. Our Celebration
         </h1>
 
         {/* Register Button */}
         <button
           onClick={handleRegisterClick}
-          className="button z-99 animated animatedFadeInUp fadeInUp mt-[70vh] z-30"
+          className="button z-99 animated animatedFadeInUp fadeInUp mt-[65vh] z-30"
         >
           R E G I S T E R
           <div id="clip">
@@ -102,7 +102,7 @@ export default function Home() {
         </button>
 
         {/* Countdown */}
-        <div className="absolute mt-[50vh] animated animatedFadeInUp fadeInUp text-white text-center px-6 py-2 rounded-lg ">
+        <div className="absolute mt-[45vh] animated animatedFadeInUp fadeInUp text-white text-center px-6 py-2 rounded-lg ">
           <Countdown targetDate="2025-08-23T00:00:00" />
         </div>
       </section>
